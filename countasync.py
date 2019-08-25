b1c0693714361765730871cec00599389c4b4588
Charles Merriam <charles.merriam@gmail.com>
2019-08-25 15:21:38 -0700

    Added some comments about what's allowed

--- countasync.py
+++ countasync.py
@@ -4,16 +4,28 @@
 import asyncio
 import time
 
+async def f(x):
+    y = await f(x)  # OK - `await` and `return` allowed in coroutines
+    # and f() must be 'awaitable' being a coroutine or implementing __await__()
+    # but "yield from z()" is not allowed
+    return y
+
+async def g(x):
+    yield x  # OK - this is an async generator
+
+
 async def count(name=""):
+    """ native coroutine, modern syntax """
     print(f"One {name}")
-    await asyncio.sleep(1)
+    await asyncio.sleep(1)  # must be 'awaitable', a coroutine or having __await__
     print(f"Two {name}")
     return f"{name} completed"
 
 @asyncio.coroutine
-async def count_co(name=""):
+def count_co(name=""):
+    """Generator-based coroutine, older syntax.  Going in Python 3.10. """
     print(f"One {name}")
-    await asyncio.sleep(1)
+    yield from asyncio.sleep(1)  # "yield from" instead of "await" because older syntax
     print(f"Two {name}")
     return f"{name} completed"
 
@@ -25,6 +37,7 @@ async def main():
     return "THIS VALUE RETURNED BY asyncio.run"
 
 if __name__ == "__main__":
+    # note that "await counter()" not allowed because this is not a 'async def' / awaitable
     s = time.perf_counter()
     r = asyncio.run(main())
     print(r)
