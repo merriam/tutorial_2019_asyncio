3d677f67e3bb151b0d996ede1b25128570006ab2
Charles Merriam <charles.merriam@gmail.com>
2019-08-25 13:33:49 -0700

    Added some return value play

--- countasync.py
+++ countasync.py
@@ -4,18 +4,29 @@
 import asyncio
 import time
 
-async def count():
-    print("One")
+async def count(name=""):
+    print(f"One {name}")
     await asyncio.sleep(1)
-    print("Two")
+    print(f"Two {name}")
+    return f"{name} completed"
 
+@asyncio.coroutine
+async def count_co(name=""):
+    print(f"One {name}")
+    await asyncio.sleep(1)
+    print(f"Two {name}")
+    return f"{name} completed"
 
 async def main():
-    await asyncio.gather(count(), count(), count())
-
+    x = count("x")  # does nothing but make a new coroutine object.  No autostart.
+    rets = await asyncio.gather(count('a'), count('b'), count('c'), count_co('d'), count_co('e'), x)
+    # this returns a coroutine object
+    print("async return return values: ", rets)
+    return "THIS VALUE RETURNED BY asyncio.run"
 
 if __name__ == "__main__":
     s = time.perf_counter()
-    asyncio.run(main())
+    r = asyncio.run(main())
+    print(r)
     elapsed = time.perf_counter() - s
     print(f"{__file__} executed in {elapsed:0.2f} seconds.")
